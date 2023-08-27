ee025a7ac14eece91078c229eac39eedce6e5cde
Valerius Mattfeld <mail@valerius.me>
2023-08-27 16:27:21 +0200

    WIP: MPI

--- src/main.rs
+++ src/main.rs
@@ -93,7 +93,7 @@ async fn main() -> Result<()> {
                 let r = match node_to_rank.get(&node) {
                     Some(r) => r.clone(),
                     None => {
-                        log::error!("[{}] No rank found for node {}", rank, node);
+                        log::warn!("[{}] No rank found for node {}", rank, node);
                         continue;
                     }
                 };
@@ -105,6 +105,39 @@ async fn main() -> Result<()> {
                     .send_with_tag(&vb[..], ROOT_LEAF_VEHICLE);
                 log::debug!("[{}] Sent vehicle to rank {}", rank, r);
                 vehicle_counter += 1;
+            }
+            log::debug!("[{}] Sent {} vehicles to ranks", rank, vehicle_counter,);
+            log::debug!("[{}] Listening for incoming connections", rank);
+            loop {
+                let (_msg, status) = world.any_process().receive_vec::<u8>();
+                match status.tag() {
+                    LEAF_ROOT_VEHICLE => {
+                        log::debug!(
+                            "[{}] Received vehicle from rank {}",
+                            rank,
+                            status.source_rank()
+                        );
+                        todo!();
+                    }
+                    EDGE_LENGTH_REQUEST => {
+                        let el_req = EdgeLengthRequest::from_bytes(_msg).unwrap();
+                        let el = my_graph.edges(el_req.from).find(|e| e.1 == el_req.to);
+                        match el {
+                            Some(el) => {
+                                let v = vec![el.2.clone()];
+                                world
+                                    .process_at_rank(status.source_rank())
+                                    .send_with_tag(&v[..], EDGE_LENGTH_RESPONSE);
+                            }
+                            None => {
+                                log::error!("[{}] Received unknown message with unknown tag", rank);
+                            }
+                        }
+                    }
+                    _ => {
+                        log::error!("[{}] Received unknown message with unknown tag", rank);
+                    }
+                }
             }
         }
         rank_number => {
@@ -122,13 +155,43 @@ async fn main() -> Result<()> {
 
             loop {
                 let (msg, status) = world.any_process().receive_vec::<u8>();
-                let v = Vehicle::from_bytes(msg).unwrap();
-                log::debug!(
-                    "[{}] Received vehicle from rank {} ID {}",
-                    rank,
-                    status.source_rank(),
-                    v.id
-                );
+                match status.tag() {
+                    ROOT_LEAF_VEHICLE => {
+                        let mut v = Vehicle::from_bytes(msg).unwrap();
+                        log::debug!(
+                            "[{}] Received vehicle from rank {} ID {}",
+                            rank,
+                            status.source_rank(),
+                            v.id
+                        );
+
+                        // II.3
+                        v.marked_for_deletion = false;
+
+                        // ask root for edge length
+                        let el_req = EdgeLengthRequest {
+                            from: v.prev_id,
+                            to: v.next_id,
+                        };
+                        let buf = EdgeLengthRequest::to_bytes(el_req).unwrap();
+                        world
+                            .process_at_rank(0)
+                            .send_with_tag(&buf[..], EDGE_LENGTH_REQUEST);
+
+                        // get edge length
+                        let (el_msg, _) = world
+                            .process_at_rank(0)
+                            .receive_vec_with_tag::<f64>(EDGE_LENGTH_RESPONSE);
+
+                        // II.5
+                        v.delta += el_msg[0];
+                        // drive vehicle
+                        todo!();
+                    }
+                    _ => {
+                        log::error!("[{}] Received unknown message with unknown tag", rank);
+                    }
+                }
             }
         }
     };
--- src/mmpi/mod.rs
+++ src/mmpi/mod.rs
@@ -1,2 +1,33 @@
+use bincode::{deserialize, serialize};
+use serde::{Deserialize, Serialize};
+
+use crate::prelude::*;
+use crate::{graph::graph::OSMID, utils::MpiMessageContent};
+
+// Root to leaf vehicle sending tag
 pub const ROOT_LEAF_VEHICLE: i32 = 0x01;
-pub const _LEAF_ROOT_VEHICLE: i32 = 0x02;
+
+// Leaf to root vehicle sending tag
+pub const LEAF_ROOT_VEHICLE: i32 = 0x02;
+
+// Leaf asks root for edge length
+pub const EDGE_LENGTH_REQUEST: i32 = 0x03;
+
+// Root responds to leaf with edge length
+pub const EDGE_LENGTH_RESPONSE: i32 = 0x04;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct EdgeLengthRequest {
+    pub from: OSMID,
+    pub to: OSMID,
+}
+
+impl MpiMessageContent<EdgeLengthRequest> for EdgeLengthRequest {
+    fn to_bytes(data: EdgeLengthRequest) -> Result<Vec<u8>> {
+        Ok(serialize(&data)?)
+    }
+
+    fn from_bytes(data: Vec<u8>) -> Result<EdgeLengthRequest> {
+        Ok(deserialize(&data)?)
+    }
+}
