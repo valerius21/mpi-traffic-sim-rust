31bd5b857d22074a9299c3984c97261f76b69e51
Valerius Mattfeld <mail@valerius.me>
2023-08-30 13:41:14 +0200

    add hpc bootstrap script

---
+++ assets/preprare_hpc.sh
@@ -0,0 +1,9 @@
+!/bin/bash
+
+echo "[prepare hpc] Loading modules..."
+module load spack-user
+source $SPACK_USER_ROOT/share/spack/setup-env.sh
+spack load llvm
+module load rev/11.06
+module load mpich/ge/gcc/64/3.2
+echo "[prepare hpc] Done."
