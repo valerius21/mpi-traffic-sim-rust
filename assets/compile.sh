6bccccfb50d69713110d82cfa1ffb1b25ac78e93
Valerius Mattfeld <mail@valerius.me>
2023-09-04 20:20:15 +0200

    fix: some script bugs

--- assets/compile.sh
+++ assets/compile.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+echo "[prepare hpc] Loading modules..."
 module load spack-user
 source $SPACK_USER_ROOT/share/spack/setup-env.sh
 spack load llvm
 module load rev/11.06
-module load mpich/gc/gcc/64
+module load mpich/ge/gcc/64
+echo "[prepare hpc] Done."
 
 cd ~/mpi-traffic-sim-rust &&
 	rm -rf target &&
