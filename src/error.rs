8ad854a971b31d6b72b3c6fabed12075b71d7366
Valerius <mail@valerius.me>
2023-09-23 21:45:46 +0200

    add docs

--- src/error.rs
+++ src/error.rs
@@ -5,9 +5,6 @@ pub enum Error {
     /// For starter, to remove as code matures.
     #[error("Generic error: {0}")]
     Generic(String),
-    /// For starter, to remove as code matures.
-    // #[error("Static error: {0}")]
-    // Static(&'static str),
 
     #[error(transparent)]
     IO(#[from] std::io::Error),
@@ -15,14 +12,6 @@ pub enum Error {
     #[error("Vector is empty")]
     EmptyVector(String),
 
-    // #[error("No Weight found for: {0}")]
-    // NoWeightFound(String),
-
-    // #[error("No inner Edge")]
-    // NoInnerEdge(String),
-
-    // #[error("Element not in Vector: {0}")]
-    // ElementNotInVector(String),
     #[error("bincode error: {0}")]
     Bincode(#[from] bincode::Error),
 }
--- src/prelude.rs
+++ src/prelude.rs
@@ -5,7 +5,3 @@ pub use crate::error::Error;
 
 // Alias Result to be the crate Result.
 pub type Result<T> = core::result::Result<T, Error>;
-
-// Generic Wrapper tuple struct for newtype pattern,
-// mostly for external type to type From/TryFrom conversions
-// pub struct W<T>(pub T);
